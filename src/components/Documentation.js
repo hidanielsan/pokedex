c345adfaf7f9ef037dfd0dcc448f5616d540b783
hidanielsan <danielsandovalp@gmail.com>
2023-04-25 12:01:05 -0600

    Index and documentation updated

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -6,8 +6,9 @@ export default function Documentation() {
             <h1 className='mb-4 px-2'>Documentation, API and Modules.</h1>
             <div className="row px-2 mb-5" style={{ fontSize: "18px" }}>
                 <p>Hi, I'm Daniel. I'm a Software Engineer. <a href="https://www.linkedin.com/in/dsandovalp/" target="_blank" rel="noreferrer">My LinkedIn</a></p>
-                <p>I made this app using React.js, JavaScript, HTML5, CSS3 and API integration.</p>
-                <p>This app started as a project for the University (<a href="https://6380c7eada05a61aa74a4f50--pokedex-by-daniel.netlify.app/" target="_blank" rel="noreferrer">this was the first version</a>), but then I gave it some extra hours to make it more complete and functional. I will add more features in the future.</p>
+                <p>I made this app using React.js, JavaScript, HTML5, CSS3 and API integration. To create this single-page React application I also used "Create React App" (npx create-react-app pokedex)</p>
+                <p>This app started as a project for the University, but then I gave it some extra hours to make it more complete and functional. I will add more features in the future.</p>
+                <p><strong>Github Repository:</strong> <a href="https://github.com/hidanielsan/pokedex" target="_blank" rel="noreferrer">https://github.com/hidanielsan/pokedex</a></p>
                 <h2>RESTful API:</h2>
                 <ul className="custom-list-docs">
                     <li>
@@ -28,6 +29,7 @@ export default function Documentation() {
                     <li className="">useContext</li>
                     <li className="">useEffect</li>
                 </ul>
+                
             </div>
         </div>
     );
